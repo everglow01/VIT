5c1c12d418556cce1c724f2eb4c36216318ed53c
Owen <everglow818@outlook.com>
2026-04-11 18:49:39 +0800

    add interface

--- model/__init__.py
+++ model/__init__.py
@@ -3,3 +3,6 @@ from .vit_model import *
 from .swin_model import *
 from .detection_head import *
 from .segmentation_head import *
+from .detr_head import *
+from .mask_branch import *
+from .swin_detr import *
